3ceed58029c715ec03c17cb65e9744c45a041505
Benedek Benkó <beni200306@gmail.com>
2025-04-19 23:00:56 +0200

    táblázat stílus változtatva

--- FRONTEND/js.js
+++ FRONTEND/js.js
@@ -2,14 +2,23 @@ async function analyzeText()
 {
     
     let inputText = document.querySelector('#inputText').value
-    const response = await fetch("http://localhost:5087/analyzer/analyze", {
+    const response = await fetch("http://localhost:5087/analyzer", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({ text: inputText })
     });
-
-    console.log(await response.json())
+    displayStatistics(await response.json())
+}
+function displayStatistics(res)
+{
+    let table= document.createElement('table')
+    let resultsDiv=document.querySelector('#results')
+    let tr=document.createElement('tr')
+    tr.innerHTML="<th>Szavak száma</th><th>Karakterek száma</th><th>Átlagos mondat hossz</th><th>Átlagos szó hossz</th>"
+    table.appendChild(tr)
+    tr=document.createElement('tr')
+    resultsDiv.appendChild(table)
 
 }
